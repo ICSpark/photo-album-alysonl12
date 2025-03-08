ec2ed6c0b991d6ac3f7794ca5cff7d9feb4a9aae
alysonl12 <alysonlaw80@gmail.com>
2025-03-08 21:16:47 +0000

    finished the images.

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 function fix() {
     //Write your code here!
+    let coconut = document.getElementById("coconut");
+    coconut.remove()
+    let coast = document.getElementById("coast");
+    coast.remove()
+    let watermelon = document.getElementById("watermelon");
+    watermelon.remove()
+    let sunflowers = document.getElementById("sunflowers");
+    sunflowers.remove()
+
+    let tree = document.createElement("img");
+    tree.id = "tree";
+    tree.scr = "to-add/christmasTree.jpg";
+    let photos = document.getElementById("photos");
+    photos.appendChild(tree);
+
+    let snowmen = document.createElement("img");
+    snowmen.id = "snowmen";
+    snowmen.scr = "to-add/snowmen.jpg";
+    let a = document.getElementById("photos");
+    a.appendChild(snowmen);
+
+    let stockings = document.createElement("img");
+    stockings.id = "stockings";
+    stockings.scr = "to-add/stockings.jpg";
+    let b = document.getElementById("photos");
+    b.appendChild(stockings);
+
+
 }
